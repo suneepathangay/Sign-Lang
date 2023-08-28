c0e872fea77ed98cfaa7e914b226d1527f818a11
suneet <suneetpathangay@gmail.com>
2023-08-28 12:53:55 -0400

    deploy

--- app.py
+++ app.py
@@ -1,20 +1,17 @@
-import requests
-from flask import Flask, jsonify, render_template, request
-from flask_cors import CORS, cross_origin
+
+from flask import Flask
 
 
 app = Flask(__name__)
 
-CORS(app)
 
-
-@app.route('/',methods=['GET'])
+@app.route('/')
 def home():
     return "hello world"
 
 
 if( __name__ =='__main__'):
-    app.run(debug=True)
+    app.run()
 
 
 
